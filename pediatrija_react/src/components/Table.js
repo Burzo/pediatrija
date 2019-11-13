be6c3cca5d80d9f5f82382dfc0adad454c3e111d
Miha Drofenik <miha.drofenik92@gmail.com>
2019-11-13 15:22:01 +0100

    added pricing

--- pediatrija_react/src/components/Table.js
+++ pediatrija_react/src/components/Table.js
@@ -4,108 +4,13 @@ const Table = (props) => {
     return (
         <div class="flex-column-wrap">
         <div class="flex w-100">
-        <a href="#samoplacniki" class="mybtn mybtn--orange" data-toggle="collapse">Cenik storitev</a>
-        <a href="#laboratorij" class="mybtn mybtn--orange" data-toggle="collapse">Cenik lab. storitev</a>
-        <a href="#pregled" class="mybtn mybtn--orange" data-toggle="collapse">Cenik pregledov</a>
-        </div>
-        <div id="samoplacniki" class="collapse">
-        <table class="pricing-table">
-        <tr>
-            <th class="heading-tertiary">Cenik storitev za samoplačnike</th>
-            <th>Cene v €</th>
-        </tr>
-        <tr>
-        <td>Prvi preventivni ali kurativni pregled</td>
-        <td>45,00</td>
-        </tr>
-        <tr>
-        <td>Ponovni preventivni ali kurativni pregled</td>
-        <td>30,00</td>
-        </tr>
-        <tr>
-        <td>Kratek obisk v ordinaciji, posvetovanje (do 15 min.)</td>
-        <td>25,00</td>
-        </tr>
-        <tr>
-        <td>Posvetovanje ali pogovor (do 30 minut)</td>
-        <td>40,00</td>
-        </tr>
-        <tr>
-        <td>Obisk bolnika na domu do 30 min (z vožnjo)</td>
-        <td>75,00</td>
-        </tr>
-        <tr>
-        <td>vsakih nadaljnjih začetih 30 min (z vožnjo)</td>
-        <td>30,00</td>
-        </tr>
-        <tr>
-        <td>Delni pregled</td>
-        <td>25,00</td>
-        </tr>
-        <tr>
-        <td>Predpis recepta (brez pregleda)</td>
-        <td>5,00</td>
-        </tr>
-        <tr>
-        <td>Manjši posegi: - inhalacija,</td>
-        <td>10,00</td>
-        </tr>
-        <tr>
-        <td>                      - prevez rane</td>
-        <td>10,00 do 20,00</td>
-        </tr>
-        </table>
-        </div>
-        <div id="laboratorij" class="collapse">
-        <table class="pricing-table">
-        <tr>
-            <th class="heading-tertiary">Laboratorijske storitve</th>
-            <th>Cene v €</th>
-        </tr>
-        <tr>
-        <td>KKS (hemogram)</td>
-        <td>10,00</td>
-        </tr>
-        <tr>
-        <td>CRP/Hitri strep test</td>
-        <td>7,50</td>
-        </tr>
-        <tr>
-        <td>Krvni sladkor/Celokupni holesterol/Nativni urin</td>
-        <td>5,00</td>
-        </tr>
-        <tr>
-        <td>Kratko zdravniško potrdilo (opravičilo za šolo, opravičilo za šport/športno vzgojo, športni klub, o pregledu otroka, za zavod za zaposlovanje…)</td>
-        <td>5,00</td>
-        </tr>
-        <tr>
-        <td>Zdravniško potrdilo (dietna prehrana v vrtcu, šoli, prepis iz kartoteke za potrebe zavarovalnice po poškodbi, o bolezni otroka za potrebe odpovedi potovanja ali druge namene, druga potrdila za potrebe raznih postopkov na željo staršev, prepis cepilne knjižice ob izgubi ali uničenju, izpolnjevanje avstrijske knjižice mame in otroka…)</td>
-        <td>25,00</td>
-        </tr>
-        <tr>
-        <td>Pošiljanje dokumentov po pošti, faksu, elektronski pošti</td>
-        <td>5,00</td>
-        </tr>
-        <tr>
-        <td>Izposoja podaljška za dajanje inhalacijskih zdravil</td>
-        <td>5,00</td>
-        </tr>
-        <tr>
-        <td>Cepilna knjižica</td>
-        <td>2,00</td>
-        </tr>
-        <tr>
-        <td>Paket Medo</td>
-        <td>35,00</td>
-        </tr>
-        <tr>
-        <td>Paket Medo za dva otroka</td>
-        <td>55,00</td>
-        </tr>
-        </table>
+        {/* <a href="#samoplacniki" class="mybtn mybtn--orange" data-toggle="collapse">Cenik storitev</a>
+        <a href="#laboratorij" class="mybtn mybtn--orange" data-toggle="collapse">Cenik lab. storitev</a> */}
+        <a href="#cenik" class="mybtn mybtn--orange mb-1" data-toggle="collapse">Kliknite za cenik</a>
         </div>
 
-        <div id="pregled" class="collapse">
+        <div id="cenik" class="collapse">
+        <p class="pricing-table__disclaimer">Cena storitve opravljene, na željo ali zahtevo naročnika, v nočnem času med 20 in 7 uro ter ob vikendih in praznikih se poviša za 50%. Nismo zavezanec za obračun in plačilo davka na dodano vrednost.</p>
         <table class="pricing-table">
         <tr>
             <th class="heading-tertiary">Pregleh pred cepljenjem in cepljenje (občasno veljajo akcijske cene)</th>
@@ -184,8 +89,99 @@ const Table = (props) => {
         </td>
         </tr>
         </table>
+        <table class="pricing-table">
+        <tr>
+            <th class="heading-tertiary">Laboratorijske storitve</th>
+            <th>Cene v €</th>
+        </tr>
+        <tr>
+        <td>KKS (hemogram)</td>
+        <td>10,00</td>
+        </tr>
+        <tr>
+        <td>CRP/Hitri strep test</td>
+        <td>7,50</td>
+        </tr>
+        <tr>
+        <td>Krvni sladkor/Celokupni holesterol/Nativni urin</td>
+        <td>5,00</td>
+        </tr>
+        <tr>
+        <td>Kratko zdravniško potrdilo (opravičilo za šolo, opravičilo za šport/športno vzgojo, športni klub, o pregledu otroka, za zavod za zaposlovanje…)</td>
+        <td>5,00</td>
+        </tr>
+        <tr>
+        <td>Zdravniško potrdilo (dietna prehrana v vrtcu, šoli, prepis iz kartoteke za potrebe zavarovalnice po poškodbi, o bolezni otroka za potrebe odpovedi potovanja ali druge namene, druga potrdila za potrebe raznih postopkov na željo staršev, prepis cepilne knjižice ob izgubi ali uničenju, izpolnjevanje avstrijske knjižice mame in otroka…)</td>
+        <td>25,00</td>
+        </tr>
+        <tr>
+        <td>Pošiljanje dokumentov po pošti, faksu, elektronski pošti</td>
+        <td>5,00</td>
+        </tr>
+        <tr>
+        <td>Izposoja podaljška za dajanje inhalacijskih zdravil</td>
+        <td>5,00</td>
+        </tr>
+        <tr>
+        <td>Cepilna knjižica</td>
+        <td>2,00</td>
+        </tr>
+        <tr>
+        <td>Paket Medo</td>
+        <td>35,00</td>
+        </tr>
+        <tr>
+        <td>Paket Medo za dva otroka</td>
+        <td>55,00</td>
+        </tr>
+        </table>
+        <table class="pricing-table">
+        <tr>
+            <th class="heading-tertiary">Cenik storitev za samoplačnike</th>
+            <th>Cene v €</th>
+        </tr>
+        <tr>
+        <td>Prvi preventivni ali kurativni pregled</td>
+        <td>45,00</td>
+        </tr>
+        <tr>
+        <td>Ponovni preventivni ali kurativni pregled</td>
+        <td>30,00</td>
+        </tr>
+        <tr>
+        <td>Kratek obisk v ordinaciji, posvetovanje (do 15 min.)</td>
+        <td>25,00</td>
+        </tr>
+        <tr>
+        <td>Posvetovanje ali pogovor (do 30 minut)</td>
+        <td>40,00</td>
+        </tr>
+        <tr>
+        <td>Obisk bolnika na domu do 30 min (z vožnjo)</td>
+        <td>75,00</td>
+        </tr>
+        <tr>
+        <td>vsakih nadaljnjih začetih 30 min (z vožnjo)</td>
+        <td>30,00</td>
+        </tr>
+        <tr>
+        <td>Delni pregled</td>
+        <td>25,00</td>
+        </tr>
+        <tr>
+        <td>Predpis recepta (brez pregleda)</td>
+        <td>5,00</td>
+        </tr>
+        <tr>
+        <td>Manjši posegi: - inhalacija,</td>
+        <td>10,00</td>
+        </tr>
+        <tr>
+        <td>                      - prevez rane</td>
+        <td>10,00 do 20,00</td>
+        </tr>
+        </table>
         </div>
-        <p>Cena storitve opravljene, na željo ali zahtevo naročnika, v nočnem času med 20 in 7 uro ter ob vikendih in praznikih se poviša za 50%. Nismo zavezanec za obračun in plačilo davka na dodano vrednost.</p>
         </div>
     )
 }
